a04327805d74a90ce13c948490ea65b81331100b
Shaun Hartman <shartman@teamsi.com>
2018-08-24 10:36:01 -0500

    drafting math logic for progress

--- pay-back/src/components/Progress.js
+++ pay-back/src/components/Progress.js
@@ -2,6 +2,22 @@ import React from "react";
 import { formatPrice } from "../helpers";
 
 class Progress extends React.Component {
+    // TODO : Work out math logic
+    renderProgress = (key) => {
+        const debt = this.props.debts[key];
+        const count = this.props.progress[key];
+        const isOpen = debt.status === "open";
+        if(!isOpen) {
+        return      <li key={key}>
+                        Congrats {debt ? debt.name : "debt"} is Paid!
+                    </li>
+        }
+        return  <li key={key}>
+                    {count} payments @ {debt.name}
+                    {formatPrice(count * debt.amount)}
+                </li>
+    }
+
     render() {
         const progressIds = Object.keys(this.props.progress);
         const total = progressIds.reduce((prevTotal, key) => {
@@ -17,7 +33,7 @@ class Progress extends React.Component {
             <div className="order-wrap">
                 <h2>Progress</h2>
                 <ul>
-                    {progressIds.map(key => <li>{key}</li>)}
+                    {progressIds.map(this.renderProgress)}
                 </ul>
                 <div className="total">
                 Total:
