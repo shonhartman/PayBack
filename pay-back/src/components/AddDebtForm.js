1ee119f52a17134587bf7531a093a72221d372d2
Shaun Hartman <shartman@teamsi.com>
2018-08-27 11:03:33 -0500

    adding <ol> for desc items

--- pay-back/src/components/AddDebtForm.js
+++ pay-back/src/components/AddDebtForm.js
@@ -1,10 +1,27 @@
 import React from "react";
 
 class AddDebtForm extends React.Component {
+
+    state = {
+        desc : []
+    }
+
     nameRef = React.createRef();
     amountRef = React.createRef();
     statusRef = React.createRef();
     descRef = React.createRef();
+
+    createItem = (event) => {
+        if(event.key == 'Tab') {
+            event.preventDefault();
+            let desc = [...this.state.desc];
+            const item = this.descRef.current.value;
+            desc = this.state.desc.push(item);
+            this.setState([desc]);
+            console.log(this.descRef.current);
+            this.descRef.current.form.reset();
+        }
+    }
 
     createDebt = (event) => {
         // 1. stop the form from submitting
@@ -28,7 +45,14 @@ class AddDebtForm extends React.Component {
                     <option value="open">Open</option>
                     <option value="paid">Paid!</option>
                 </select>
-                <textarea name="desc" ref={this.descRef} placeholder="Desc"></textarea>
+                {/* <textarea name="desc" ref={this.descRef} placeholder="Desc"></textarea> */}
+                <input name="desc" ref={this.descRef} type="text" onKeyDown={this.createItem}  />
+                <ol>
+                    {Object.keys(this.state.desc).map(key => <li key={key}>{this.state.desc}</li>)}
+                </ol>
+                {/* <ul className="fishes">
+                        {Object.keys(this.state.debts).map(key => <Debt key={key} details={this.state.debts[key]} addPayment={this.addPayment} index={key} /> )}
+                    </ul> */}
                 <button type="submit">+ Add Debt</button>
             </form>
         )
--- pay-back/src/components/DebtSelector.js
+++ pay-back/src/components/DebtSelector.js
@@ -2,12 +2,14 @@ import React from "react";
 
 class DebtSelector extends React.Component {
     debtInput = React.createRef();
+    addressInput = React.createRef();
 
     goToDebt = (event) => {
         // 1. Stop the form from submitting
         event.preventDefault();
-        // 2. get the text from that input
+        // 2. get the text from the inputs
         const debtName = this.debtInput.current.value;
+        const address = this.addressInput.current.value;
         // 3. Change the page to /debt/whatever-they-entered
         this.props.history.push(`/debt/${debtName}`);
     }
@@ -17,6 +19,7 @@ class DebtSelector extends React.Component {
             <form className="store-selector" onSubmit={this.goToDebt}>
                 <h2>Enter a Debt</h2>
                 <input type="text" ref={this.debtInput} required placeholder="Debt Name"/>
+                <input type="text" ref={this.addressInput} required placeholder="Address"/>
                 <button type="submit">Go to Debt</button>
             </form>
         )
